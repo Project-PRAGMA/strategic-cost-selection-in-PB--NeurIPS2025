5bbd76d51bf065cd0ecebce934e222b49f3d482c
Andrzej Kaczmarczyk <droodev@gmail.com>
2025-10-28 13:45:42 +0100

    Adding two missing file that are crucial to the code.

---
+++ _glossary.py
@@ -0,0 +1,18 @@
+
+
+NAMES = {}
+
+# AMSTERDAM
+NAMES['amsterdam'] = {
+    '166.pb': 'Kleine Wereld',
+}
+
+
+# WARSZAWA
+NAMES['warszawa_2023'] = {
+    'poland_warszawa_2023_bemowo.pb': 'Bemowo',
+    'poland_warszawa_2023_bielany.pb': 'Bielany',
+    'poland_warszawa_2023_wesola.pb': 'Wesola',
+    'poland_warszawa_2023_wilanow.pb': 'Wilanow',
+    'poland_warszawa_2023_wlochy.pb': 'Wlochy',
+}
